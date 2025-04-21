c09235e7e296968c921876b2a2b0b78223b71574
Mouzheng LI <L2164753957@gmail.com>
2025-04-21 21:09:05 +0200

    change js 2 php in profile

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -83,6 +83,59 @@ switch ($action) {
         header('Location: ../views/index.html');
         exit;
 
+    case 'reset_password':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['user']) && $_POST['code_ok'] == '1') {
+            $newPwd = $_POST['new_password'];
+            $email = $_POST['email'];
+            $role = $_POST['role'];
+
+            $hashedPwd = password_hash($newPwd, PASSWORD_DEFAULT);
+            $user = $_SESSION['user'];
+            $id = ($role === 'etudiant') ? $user['IdEtudiant'] : $user['IdPropietaire'];
+
+            $data = $user;
+            $data['mdp'] = $newPwd; // reset password
+
+            if ($role === 'etudiant') {
+                $pdo->prepare("UPDATE Etudiant SET MDP = :mdp WHERE IdEtudiant = :id")
+                    ->execute([':mdp' => $hashedPwd, ':id' => $id]);
+            } elseif ($role === 'proprietaire') {
+                $pdo->prepare("UPDATE Propietaire SET MDP = :mdp WHERE IdPropietaire = :id")
+                    ->execute([':mdp' => $hashedPwd, ':id' => $id]);
+            }
+
+            echo "<script>alert('Mot de passe modifié avec succès !'); window.location.href = '../views/login.html';</script>";
+            exit;
+        } else {
+            echo "<p>Échec de la vérification. Reconnectez-vous ou code invalide.</p>";
+        }
+        break;
+
+    case 'update_profile':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['user'])) {
+            $user = $_SESSION['user'];
+            $role = $_POST['role'] ?? $_SESSION['role'];
+            $photoPath = null;
+
+            if (isset($_FILES['photo']) && $_FILES['photo']['error'] === 0) {
+                if ($role === 'etudiant') {
+                    $photoPath = UserModel::enregistrerPhotoEtudiant($user['IdEtudiant'], $_FILES['photo']);
+                } elseif ($role === 'proprietaire') {
+                    $photoPath = UserModel::enregistrerPhotoProprietaire($user['IdPropietaire'], $_FILES['photo']);
+                }
+            }
+
+            if ($role === 'etudiant') {
+                UserModel::updateEtudiant($user['IdEtudiant'], $_POST, $photoPath);
+            } elseif ($role === 'proprietaire') {
+                UserModel::updateProprietaire($user['IdPropietaire'], $_POST, $photoPath);
+            }
+
+            echo "<script>alert('Profil mis à jour !'); window.location.href = '../views/index2.html';</script>";
+            exit;
+        }
+        break;
+
     default:
         echo "<p>Aucune action valide spécifiée.</p>";
         break;

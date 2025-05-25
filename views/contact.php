c5900849e6c0138cefc049093f90e145e264f05c
Mouzheng LI <L2164753957@gmail.com>
2025-05-25 21:21:42 +0200

    debug

--- views/contact.php
+++ views/contact.php
@@ -5,36 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Contact</title>
   <link rel="stylesheet" href="/views/contact.css">
-  
 </head>
 <body>
   <?php include __DIR__ . '/header.php'; ?>
 
   <div class="contact-card">
     <h2>📬 Contactez-nous</h2>
-    <form action="submit_contact.php" method="POST">
-      <div class="form-group">
-        <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" required>
-      </div>
-
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" required>
-      </div>
-
-      <div class="form-group">
-        <label for="sujet">Sujet</label>
-        <input type="text" name="sujet" id="sujet" required>
-      </div>
-
-      <div class="form-group">
-        <label for="message">Message</label>
-        <textarea name="message" id="message" required></textarea>
-      </div>
-
-      <button class="submit-btn" type="submit">Envoyer</button>
-    </form>
+    <div class="contact-info">
+      <p>📧 Email : Home4Student@gmail.com</p>
+      <p>📞 Téléphone : 06 12 76 47 46</p>
+    </div>
   </div>
   <?php include __DIR__ . '/footer.html'; ?>
 </body>
